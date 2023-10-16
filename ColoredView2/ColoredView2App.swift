a0822eae6dd4c38140f95ef03a079d2ad575ece1
Vadim Vanatov <79990006611@yandex.ru>
2023-10-16 22:47:50 +0300

    Initial Commit

---
+++ ColoredView2/ColoredView2App.swift
@@ -0,0 +1,17 @@
+//
+//  ColoredView2App.swift
+//  ColoredView2
+//
+//  Created by HOLY NADRUGANTIX on 16.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColoredView2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
